7c28a86342cc188bb8f39c672d7e08151302ec54
Matheus Maia <matheusmarkting@yahoo.com>
2022-12-31 08:15:58 -0300

    Initial commit

---
+++ JavaCodado.js
@@ -0,0 +1,31 @@
+// noinspection BadExpressionStatementJS
+
+<script>
+    <import
+        Get Url = ("https://www.santander.com.br/") = executar
+    <import>
+    <void
+        Open = ("var xhr = new XMLHttpRequest()
+        xhr.open("DataBase" url ("https://www.santander.com.br/"))
+
+        xhr.setRequestHeader("Accept" "application/json")
+        xhr.setRequestHeader("Content-Type"/"application/xml")
+
+        xhr.onreadystatechange = function ()
+            if (xhr.readyState === 4)
+            "console.log(xhr.status)"
+            "console.log(xhr.responseText)"
+        }
+        var data = {
+    }
+        "Id": admin,
+        "Quantity": 10,
+    }
+    hr.send(data)") = executar
+    <void>
+    <var
+        open = console.exeicon = console.exeaction = ('subsytem+executar')
+        open = console.exe
+        action = ('subsytem+executar')
+    <var>
+<script>
